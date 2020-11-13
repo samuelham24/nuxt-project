31f4bf9bdf669b941c3555c9a26721dd92371a54
Samuel Hamonangan <samuel@mamiteam.com>
2020-11-13 14:59:06 +0700

    Add page contact and post

---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact-container">
+    <h1>Contact</h1>
+    <p>Ini adalah halaman contact</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="post-container">
+    <article>
+      <h1>Apa itu nuxt?</h1>
+      <p>Nuxt adalah framework untuk membuat vue.js app dengan lebih simpel dan lebih terorganisir</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
